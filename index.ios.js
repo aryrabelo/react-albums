147e8d8ad5edad093375d89bb5dd5f801e88f492
Ary Rabelo <aryrabelo@gmail.com>
2017-03-18 15:20:22 -0300

    first commit

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+// Code for IOS!!!!
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+const App = () => (
+    <Header />
+);
+
+AppRegistry.registerComponent('albums', () => App);
